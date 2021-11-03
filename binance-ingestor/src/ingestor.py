0f72fbb28ccd973063b24b104090f120b3519703
Justin Guese <guese.justin@gmail.com>
2021-11-03 10:03:46 +0100

    removing volume and pctofmarketcap for now

--- binance-ingestor/src/ingestor.py
+++ binance-ingestor/src/ingestor.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
             # update
             SYMBOLS = { "BTCUSDT" : 1196358476445, "ETHUSDT" : 532988896161, "BNBUSDT" : 94580743908 }
             for SYMBOL, MARKETCAP in SYMBOLS.items():
-                trades = client.get_recent_trades(symbol= SYMBOL, limit = 100000)
+                trades = client.get_recent_trades(symbol= SYMBOL, limit = 100000) # 1k max
                 df = pd.DataFrame(trades)
                 df["symbol"] = SYMBOL
                 df['time'] = pd.to_datetime(df['time'],unit='ms')
@@ -26,10 +26,11 @@ if __name__ == '__main__':
                 df["price"] = df["price"].astype(float)
                 df["qty"] = df["qty"].astype(float)
                 df["quoteQty"] = df["quoteQty"].astype(float)
-                df["volume"] = df["qty"] * df["price"]
+                # actually leave out volume and pctOfMarketcap as we save storage space then
+                # df["volume"] = df["qty"] * df["price"]
                 df["id"] = df["id"].astype(str)
                 df["id"] = df["id"] + df["symbol"]
-                df["pctOfMarketcap"] = df["qty"] / MARKETCAP * 1000000000
+                # df["pctOfMarketcap"] = df["qty"] / MARKETCAP * 1000000000
 
                 # print(df.head())
                 print("saving data for %s" % SYMBOL)
